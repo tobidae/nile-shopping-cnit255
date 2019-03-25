7a22fd11307003a219b8bf7e515d831d4a4c14d3
Tobi Akerele <otakerele@gmail.com>
2019-03-25 14:58:15 -0400

    rename the tabs

--- app/src/main/java/purdue/cnit255/shoppingapp/MainActivity.java
+++ app/src/main/java/purdue/cnit255/shoppingapp/MainActivity.java
@@ -17,14 +17,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_product:
+                    mTextMessage.setText(R.string.title_product);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_user:
+                    mTextMessage.setText(R.string.title_user);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_cart:
+                    mTextMessage.setText(R.string.title_cart);
                     return true;
             }
             return false;
