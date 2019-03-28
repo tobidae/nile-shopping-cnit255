3ee83eb36584e570dd6e8248b7d4eaa489a79bcc
Tobi Akerele <otakerele@gmail.com>
2019-03-27 20:47:32 -0400

    All the fragments for Cart, Product and User

--- app/src/main/java/purdue/cnit255/shoppingapp/MainActivity.java
+++ app/src/main/java/purdue/cnit255/shoppingapp/MainActivity.java
@@ -1,15 +1,16 @@
 package purdue.cnit255.shoppingapp;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,13 +19,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_product:
-                    mTextMessage.setText(R.string.title_product);
+                    pushFragment(new ProductFragment());
                     return true;
                 case R.id.navigation_user:
-                    mTextMessage.setText(R.string.title_user);
+                    pushFragment(new UserFragment());
                     return true;
                 case R.id.navigation_cart:
-                    mTextMessage.setText(R.string.title_cart);
+                    pushFragment(new CartFragment());
                     return true;
             }
             return false;
@@ -36,9 +37,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // Select first menu item by default and show Fragment accordingly.
+        Menu menu = navigation.getMenu();
+        pushFragment(new ProductFragment());
+
     }
+
+    /**
+     * Method to push any fragment into given id.
+     *
+     * @param fragment An instance of Fragment to show into the given id.
+     */
+    protected void pushFragment(Fragment fragment) {
+        if (fragment == null)
+            return;
+
+        FragmentManager fragmentManager = getFragmentManager();
+        if (fragmentManager != null) {
+            FragmentTransaction ft = fragmentManager.beginTransaction();
+            if (ft != null) {
+                ft.replace(R.id.rootLayout, fragment);
+                ft.commit();
+            }
+        }
+    }
+
 
 }
---
+++ app/src/main/java/purdue/cnit255/shoppingapp/UserFragment.java
@@ -0,0 +1,16 @@
+package purdue.cnit255.shoppingapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UserFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.user_fragment, container, false);
+    }
+}
