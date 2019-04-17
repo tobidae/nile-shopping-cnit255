63fc3fea3f7ea1c7460e913274e70fda1622e604
Iris Chu <chuiris17@gmail.com>
2019-04-17 16:20:39 -0400

    created some classes, pushing to master.

--- app/src/main/java/purdue/cnit255/shoppingapp/Helpers/Cart.java
+++ app/src/main/java/purdue/cnit255/shoppingapp/Helpers/Cart.java
@@ -1,4 +1,10 @@
 package purdue.cnit255.shoppingapp.Helpers;
 
+import java.util.ArrayList;
+
 public class Cart {
+    ArrayList<Product> items;
+    Customer user;
+
+    public Cart(Customer user)
 }
