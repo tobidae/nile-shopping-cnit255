f0c49cdcb64d2af5cbf64bb43057c4228570c255
Iris Chu <chuiris17@gmail.com>
2019-04-18 09:12:41 -0400

    created constructor for Cart

--- app/src/main/java/purdue/cnit255/shoppingapp/Helpers/Cart.java
+++ app/src/main/java/purdue/cnit255/shoppingapp/Helpers/Cart.java
@@ -3,8 +3,10 @@ package purdue.cnit255.shoppingapp.Helpers;
 import java.util.ArrayList;
 
 public class Cart {
-    ArrayList<Product> items;
+    ArrayList<Product> items =  new ArrayList<Product>();
     Customer user;
 
-    public Cart(Customer user)
+    public Cart(Customer user){
+        this.user = user;
+    }
 }
