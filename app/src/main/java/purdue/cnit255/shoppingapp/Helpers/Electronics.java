b5672eb703ae39ad52d0d4cfd62785a9882d775f
LandanPerry <ldperry16@gmail.com>
2019-04-17 22:55:18 -0400

    Updated Electronics Class

--- app/src/main/java/purdue/cnit255/shoppingapp/Helpers/Electronics.java
+++ app/src/main/java/purdue/cnit255/shoppingapp/Helpers/Electronics.java
@@ -1,4 +1,72 @@
 package purdue.cnit255.shoppingapp.Helpers;
 
 public class Electronics extends Product {
+
+  private int amountAvailable;
+
+     public Electronic(String itemName, String condition, String itemDescription, String manufacturer, float price,int amountAvailable) {
+         super(itemName, condition, itemDescription, manufacturer, price);
+         this.amountAvailable=amountAvailable;
+     }
+
+     public int getAmountAvailable() {
+         return amountAvailable;
+     }
+
+     public void setAmountAvailable(int amountAvailable) {
+         this.amountAvailable = amountAvailable;
+     }
+
+     @Override
+     public String getCondition() {
+         return super.getCondition();
+     }
+
+     @Override
+     public String getItemDescription() {
+         return super.getItemDescription();
+     }
+
+     @Override
+     public float getPrice() {
+         return super.getPrice();
+     }
+
+     @Override
+     public String getItemName() {
+         return super.getItemName();
+     }
+
+     @Override
+     public String getManufacturer() {
+         return super.getManufacturer();
+     }
+
+     @Override
+     public void setCondition(String condition) {
+         super.setCondition(condition);
+     }
+
+     @Override
+     public void setItemDescription(String itemDescription) {
+         super.setItemDescription(itemDescription);
+     }
+
+     @Override
+     public void setItemName(String itemName) {
+         super.setItemName(itemName);
+     }
+
+     @Override
+     public void setManufacturer(String manufacturer) {
+         super.setManufacturer(manufacturer);
+     }
+
+     @Override
+     public void setPrice(float price) {
+         super.setPrice(price);
+     }
+
+
+
 }
