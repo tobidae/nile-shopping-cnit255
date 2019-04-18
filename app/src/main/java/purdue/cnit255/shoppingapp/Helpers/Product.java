ddd9a9766f7c2358069bcd11cac4e5e6f014baf9
LandanPerry <ldperry16@gmail.com>
2019-04-17 22:39:09 -0400

    Updated Product Class

--- app/src/main/java/purdue/cnit255/shoppingapp/Helpers/Product.java
+++ app/src/main/java/purdue/cnit255/shoppingapp/Helpers/Product.java
@@ -1,4 +1,61 @@
 package purdue.cnit255.shoppingapp.Helpers;
 
 public class Product {
+
+  private String itemName;
+     private String condition;
+     private String itemDescription;
+     private String manufacturer;
+     private float price;
+
+     public Product(String itemName,String condition,String itemDescription, String manufacturer, float price){
+         this.itemName=itemName;
+         this.condition=condition;
+         this.itemDescription=itemDescription;
+         this.manufacturer=manufacturer;
+         this.price=price;
+
+     }
+
+     public void setCondition(String condition) {
+         this.condition = condition;
+     }
+
+     public String getCondition() {
+         return condition;
+     }
+
+     public String getItemDescription() {
+         return itemDescription;
+     }
+
+     public String getItemName() {
+         return itemName;
+     }
+
+     public String getManufacturer() {
+         return manufacturer;
+     }
+
+     public void setItemDescription(String itemDescription) {
+         this.itemDescription = itemDescription;
+     }
+
+     public void setItemName(String itemName) {
+         this.itemName = itemName;
+     }
+
+     public void setManufacturer(String manufacturer) {
+         this.manufacturer = manufacturer;
+     }
+
+     public void setPrice(float price) {
+         this.price = price;
+     }
+
+     public float getPrice() {
+         return price;
+     }
+
+
 }
